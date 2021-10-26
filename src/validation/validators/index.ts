b1066102e9affef555a69c6a6e3f0e19a114a536
alioshmpb@gmail.com <alioshmpb@gmail.com>
2021-10-26 17:33:25 -0300

    fix: remove composite & builder exports from validation layer

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,4 +1,2 @@
-export * from './composite/validator-composite'
 export * from './min-length/min-length-validator'
 export * from './required/required-field-validation'
-export * from './builder/validation-builder'
