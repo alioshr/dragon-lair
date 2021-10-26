417d360769d47a29d9731b6d90a1245b8cbc4d94
“alioshr” <alioshmpb@gmail.com>
2021-10-26 11:34:49 -0300

    test: ensure to call Validator with the correct name

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -6,6 +6,30 @@ import { ValidationSpy } from '@/presentation/test'
 import faker from 'faker'
 
 const VALIDATION_ERROR_MESSAGE = faker.random.words(2)
+const CREDENTIALS = {
+  name: faker.random.words(),
+  password: faker.internet.password()
+}
+
+const makeValidationSpyAssertion = (
+  validationSpy: ValidationSpy,
+  sut: RenderResult,
+  fieldName: string,
+  credentials: any
+): void => {
+  const validatorSpy = jest.spyOn(validationSpy, 'validate')
+  fieldName === 'name'
+    ? Helper.populateField(sut, 'name', CREDENTIALS.name)
+    : Helper.populateField(sut, 'password', CREDENTIALS.password)
+
+  expect(validatorSpy).toHaveBeenCalledWith(
+    fieldName,
+    credentials
+  )
+  expect(validationSpy.inputData).toEqual(
+    credentials
+  )
+}
 
 type SutTypes = {
   sut: RenderResult
@@ -25,5 +49,9 @@ describe('Login', () => {
     Helper.testButtonDisabled(sut, 'submit-button', true)
     Helper.testStatusForField(sut, 'name', 'default', VALIDATION_ERROR_MESSAGE)
     Helper.testStatusForField(sut, 'password', 'default', VALIDATION_ERROR_MESSAGE)
+  })
+  test('Should call validation with the correct name', () => {
+    const { sut, validatorSpy } = makeSut(VALIDATION_ERROR_MESSAGE)
+    makeValidationSpyAssertion(validatorSpy, sut, 'name', { name: CREDENTIALS.name, password: '' })
   })
 })
