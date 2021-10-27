7d764c2df7ab19b4d136a6b5cc812d198d75aa47
“alioshr” <alioshmpb@gmail.com>
2021-10-27 01:10:35 -0300

    test: add axios adapter

---
+++ src/infra/http/axios/axios-adapter.spec.ts
@@ -0,0 +1,63 @@
+import { AxiosAdapter } from './axios-adapter'
+import axios from 'axios'
+import { mockedAxiosResponse } from '@/infra/tests'
+import {
+  mockedBody,
+  mockedHeaders,
+  mockedHttpRequest,
+  mockedHttpResponse,
+  mockedUrl
+} from '@/data/tests'
+import { HttpRequest } from '@/data/protocols'
+
+jest.mock('axios', () => ({
+  request: () => mockedAxiosResponse(mockedHttpResponse)
+}))
+
+const URL = mockedUrl()
+const BODY = mockedBody()
+const REQUEST_PARAMS = mockedHttpRequest({
+  url: URL,
+  body: BODY,
+  method: 'POST',
+  headers: mockedHeaders()
+})
+
+type SutTypes = {
+  sut: AxiosAdapter<any, any>
+}
+
+const makeSut = (requestParams?: HttpRequest): SutTypes => {
+  const sut = new AxiosAdapter()
+  return { sut }
+}
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with the correct params on post', async () => {
+    const { sut } = makeSut()
+    const postSpy = jest.spyOn(axios, 'request')
+    await sut.request(REQUEST_PARAMS)
+    expect(postSpy).toHaveBeenCalledWith({
+      url: REQUEST_PARAMS.url,
+      method: REQUEST_PARAMS.method,
+      data: REQUEST_PARAMS.body,
+      headers: REQUEST_PARAMS.headers
+    })
+  })
+  test('Should return a proper HttpResponse on success on post', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.request(REQUEST_PARAMS)
+    expect(httpResponse).toEqual(mockedHttpResponse)
+  })
+  test('Should return a proper HttpResponse on failure', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(axios, 'request').mockRejectedValueOnce({
+      response: mockedAxiosResponse(mockedHttpResponse)
+    })
+    const result = await sut.request(REQUEST_PARAMS)
+    expect(result).toEqual({
+      statusCode: mockedHttpResponse.statusCode,
+      body: mockedHttpResponse.body
+    })
+  })
+})
