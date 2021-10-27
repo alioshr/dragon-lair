95205d2dd94eba4ffb5d87b7270851951c657ec3
“alioshr” <alioshmpb@gmail.com>
2021-10-27 00:58:20 -0300

    test: ensure to throw UnexpectedError if HttpClient return 500

--- src/data/usecases/get-dragons/remote-get-dragons.spec.ts
+++ src/data/usecases/get-dragons/remote-get-dragons.spec.ts
@@ -45,4 +45,11 @@ describe('RemoteGetDragons', () => {
     const promise = sut.get(params)
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+  test('should throw Unexpected error HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = { statusCode: HttpStatusCode.serverError }
+    const params = faker.datatype.uuid()
+    const promise = sut.get(params)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
