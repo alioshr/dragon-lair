4cc702b6665e46963ea22fef32ff11dfd875e089
“alioshr” <alioshmpb@gmail.com>
2021-10-28 19:39:33 -0300

    feat: ensure to call httpClient with the correct url and method

---
+++ src/data/usecases/exclude-dragon/remote-exclude-dragons.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@/data/protocols'
+import { ExcludeDragon } from '@/domain/usecases/exclude-dragon'
+
+export class RemoteExcludeDragon implements ExcludeDragon {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient
+  ) {}
+
+  async delete (id: string): Promise<void> {
+    await this.httpClient.request({
+      url: this.url,
+      method: 'DELETE'
+    })
+    return await Promise.resolve(null as any)
+  }
+}
