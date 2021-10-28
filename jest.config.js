6a0d30839001212d25ae79b3486bdd022636c73a
“alioshr” <alioshmpb@gmail.com>
2021-10-28 19:30:52 -0300

    chore: remove main layer, router and errors from coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/presentation/router/**/*',
+    '!<rootDir>/src/validation/errors/**/*',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
