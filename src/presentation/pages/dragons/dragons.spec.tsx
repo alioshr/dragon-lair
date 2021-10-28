5558728b07133a28b35830c292e23b0775561e06
“alioshr” <alioshmpb@gmail.com>
2021-10-28 02:42:41 -0300

    test: ensure to render dragons on success

--- src/presentation/pages/dragons/dragons.spec.tsx
+++ src/presentation/pages/dragons/dragons.spec.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, screen, waitFor } from '@testing-library/react'
 import { Dragons } from '..'
+import { GetDragons } from '@/domain/usecases/get-dragons'
+import { Dragon } from '@/domain/models'
+import { mockedDragons } from '@/domain/test'
+
+class GetDragonsSpy implements GetDragons {
+  callCount = 0
+  id: string | undefined = ''
+  dragons = mockedDragons()
+  async get (id?: string): Promise<Dragon[]> {
+    this.id = id
+    this.callCount = this.callCount + 1
+    return await Promise.resolve(this.dragons)
+  }
+}
 
 type SutTypes = {
   sut: RenderResult
+  getDragonsSpy: GetDragonsSpy
 
 }
 const makeSut = (): SutTypes => {
-  const sut = render(<Dragons />)
-  return { sut }
+  const getDragonsSpy = new GetDragonsSpy()
+  const sut = render(<Dragons getDragons={getDragonsSpy}/>)
+  return { sut, getDragonsSpy }
 }
 describe('Dragons', () => {
   test('Should load 4 skeletons initially', () => {
@@ -16,5 +32,14 @@ describe('Dragons', () => {
     const dragonList = sut.getByTestId('dragons-list')
     const skeletons = dragonList.querySelectorAll('li:empty')
     expect(skeletons.length).toBe(4)
+  })
+  test('should render Dragons on success', async () => {
+    makeSut()
+    const dragonList = screen.getByTestId('dragons-list')
+    await waitFor(() => dragonList)
+    const dragons = dragonList.querySelectorAll('div.dragonContent')
+    const skeletons = dragonList.querySelectorAll('li:empty')
+    expect(dragons).toHaveLength(3)
+    expect(skeletons.length).toBe(0)
   })
 })
