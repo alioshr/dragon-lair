8345acc56ca622f2bce330ec324224f3ea54300d
“alioshr” <alioshmpb@gmail.com>
2021-10-28 18:56:17 -0300

    feat: inject root composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from '@/presentation/router/router'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
+import { makeDragons } from './factories/pages/dragons/dragons-factory'
 
 ReactDOM.render(
-  <Router makeLogin={makeLogin}/>,
+  <Router makeLogin={makeLogin} makeDragons={makeDragons}/>,
   document.getElementById('main')
 )
