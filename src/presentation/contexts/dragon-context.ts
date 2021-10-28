2986e481dd35180a68a7963379773fa6fb07534c
“alioshr” <alioshmpb@gmail.com>
2021-10-28 18:26:37 -0300

    feat: add dragons context

---
+++ src/presentation/contexts/dragon-context.ts
@@ -0,0 +1,15 @@
+import { Dragon } from '@/domain/models'
+import { createContext, Dispatch, SetStateAction } from 'react'
+
+export type StateTypes = {
+  dragons: Dragon[]
+  isLoading: boolean
+  error: null | string
+}
+
+export type DragonStateTypes = {
+  state: StateTypes
+  setState: Dispatch<SetStateAction<StateTypes>>
+}
+
+export default createContext<DragonStateTypes>(null as any)
