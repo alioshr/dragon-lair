84b914c32fd8836ca0089c7213b47beb584bafbe
“alioshr” <alioshmpb@gmail.com>
2021-10-28 22:28:51 -0300

    test: ensure RemoteUpdateDragon throws if HttpClient returns 400

--- src/data/usecases/update-dragon/remote-update-dragon.spec.ts
+++ src/data/usecases/update-dragon/remote-update-dragon.spec.ts
@@ -1,4 +1,6 @@
+import { HttpStatusCode } from '@/data/protocols'
 import { mockedUrl, HttpClientSpy } from '@/data/tests'
+import { UnexpectedError } from '@/domain/errors'
 import { mockedUpdateDragonDTO } from '@/domain/test'
 import { UpdateDragon } from '@/domain/usecases/update-dragon'
 import { RemoteUpdateDragon } from './remote-update-dragon'
@@ -24,5 +26,11 @@ describe('RemoteUpdateDragon', () => {
     expect(httpClientSpy.url).toBe(URL)
     expect(httpClientSpy.method).toBe('PUT')
     expect(httpClientSpy.body).toEqual(UPDATE_PARAMS.body)
+  })
+  test('should throw Unexpected error HttpClient returns 400', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = { statusCode: HttpStatusCode.badRequest }
+    const promise = sut.update(UPDATE_PARAMS)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
