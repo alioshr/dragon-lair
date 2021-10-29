a31f671b13b23f446b350ce69cf328abbf2114dd
“alioshr” <alioshmpb@gmail.com>
2021-10-28 21:45:08 -0300

    feat: add RemoteExcludeDragon usecase factory

---
+++ src/main/factories/usecases/exclude-dragon/remote-exclude-dragon-factory.ts
@@ -0,0 +1,9 @@
+import { RemoteExcludeDragon } from '@/data/usecases/exclude-dragon/remote-exclude-dragons'
+import { ExcludeDragon } from '@/domain/usecases'
+import { HttpDragonIdDecorator } from '@/main/decorators/http-dragon-id-decorator'
+import { makeAPIUrl } from '../../http/api-url-factory'
+import { makeAxiosHttpClient } from '../../http/axios-client-factory'
+
+export const makeRemoteExcludeDragon = (): ExcludeDragon => {
+  return new RemoteExcludeDragon(makeAPIUrl('/dragon'), new HttpDragonIdDecorator(makeAxiosHttpClient()))
+}
