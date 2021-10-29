d77654e6a91aaf81ba3a6376dd75ea8de8f42561
“alioshr” <alioshmpb@gmail.com>
2021-10-28 21:45:35 -0300

    feat: inject usecase into dragon factory

--- src/main/factories/pages/dragons/dragons-factory.tsx
+++ src/main/factories/pages/dragons/dragons-factory.tsx
@@ -1,9 +1,11 @@
 import { Dragons } from '@/presentation/pages'
 import React from 'react'
+import { makeRemoteExcludeDragon } from '../../usecases/exclude-dragon/remote-exclude-dragon-factory'
 import { makeRemoteGetDragons } from '../../usecases/get-dragons/remote-get-dragons-factory'
 
 export const makeDragons: React.FC = () => (
   <Dragons
+  excludeDragon={makeRemoteExcludeDragon()}
   getDragons={makeRemoteGetDragons()}
   />
 )
