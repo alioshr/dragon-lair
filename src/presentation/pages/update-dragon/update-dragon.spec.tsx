568ef6353ad26af51f1af6f996fb02fe44b0616b
“alioshr” <alioshmpb@gmail.com>
2021-10-29 10:06:39 -0300

    test: ensure to present a populated form on success

--- src/presentation/pages/update-dragon/update-dragon.spec.tsx
+++ src/presentation/pages/update-dragon/update-dragon.spec.tsx
@@ -2,10 +2,11 @@ import { mockedDragons } from '@/domain/test'
 import { GetDragonSpy } from '@/presentation/test/get-dragon-spy'
 import React from 'react'
 import { UpdateDragon } from '..'
-import { render, RenderResult, screen } from '@testing-library/react'
+import { render, RenderResult, screen, waitFor } from '@testing-library/react'
 import { createMemoryHistory } from 'history'
 import { Router } from 'react-router'
 import { Route } from 'react-router-dom'
+import * as Helper from '@/presentation/test/form-helper'
 
 const DRAGON = mockedDragons()[0]
 
@@ -38,8 +39,17 @@ describe('UpdateDragon', () => {
     const wrapper = screen.getByTestId('wrapper')
     const skeleton = wrapper.querySelector('div.skeleton')
     const form = wrapper.querySelector('form')
-
     expect(skeleton).toBeTruthy()
     expect(form).toBeNull()
+  })
+  test('should present populated form on success with button disabled', async () => {
+    makeSut()
+    const wrapper = screen.getByTestId('wrapper')
+    await waitFor(() => wrapper)
+    const nameInput = screen.getByTestId('name-input')
+    const typeInput = screen.getByTestId('type-input')
+    expect((nameInput as HTMLInputElement).value).toBe(DRAGON.name)
+    expect((typeInput as HTMLInputElement).value).toBe(DRAGON.type)
+    Helper.testButtonDisabled('submit-button', true)
   })
 })
