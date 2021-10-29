8488cdda686f6a620ffe9fdbfadc52eb4819309a
“alioshr” <alioshmpb@gmail.com>
2021-10-29 14:09:45 -0300

    feat: inject page

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom'
 import Router from '@/presentation/router/router'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeDragons } from './factories/pages/dragons/dragons-factory'
+import { makeUpdateDragon } from './factories/pages/update-dragon/update-dragon-factory'
 
 ReactDOM.render(
-  <Router makeLogin={makeLogin} makeDragons={makeDragons}/>,
+  <Router
+  makeLogin={makeLogin}
+  makeDragons={makeDragons}
+  updateDragon={makeUpdateDragon}
+  />,
   document.getElementById('main')
 )
