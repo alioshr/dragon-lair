5119f137a7706afb3ac4987b00e87aa5baeface1
“alioshr” <alioshmpb@gmail.com>
2021-10-29 14:56:02 -0300

    feat: compose page

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -4,12 +4,14 @@ import Router from '@/presentation/router/router'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeDragons } from './factories/pages/dragons/dragons-factory'
 import { makeUpdateDragon } from './factories/pages/update-dragon/update-dragon-factory'
+import { makeDetailsPage } from './factories/pages/details/details-page-factory'
 
 ReactDOM.render(
   <Router
   makeLogin={makeLogin}
   makeDragons={makeDragons}
   updateDragon={makeUpdateDragon}
+  detailsPage={makeDetailsPage}
   />,
   document.getElementById('main')
 )
