4358ab6bbcd9d16f30a0abe22956ac94d4d37dba
“alioshr” <alioshmpb@gmail.com>
2021-10-29 01:11:06 -0300

    feat: create login context

---
+++ src/presentation/contexts/login-context.ts
@@ -0,0 +1,19 @@
+import { createContext, Dispatch, SetStateAction } from 'react'
+
+export type StateTypes = {
+  password: string
+  name: string
+}
+
+export type ErrorStateTypes = {
+  password: string | null
+  name: string | null
+  main: string | null
+}
+
+export type FormStateTypes = {
+  state: [StateTypes, Dispatch<SetStateAction<StateTypes>>]
+  errorState: [ErrorStateTypes, Dispatch<SetStateAction<ErrorStateTypes>>]
+}
+
+export default createContext<FormStateTypes>(null as any)
