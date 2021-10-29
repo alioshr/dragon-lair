3dee13310f713717914069ebde47f1eb5c51ad82
“alioshr” <alioshmpb@gmail.com>
2021-10-29 14:00:51 -0300

    feat: add spinner to form status component

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,3 +1,5 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { Context, useContext } from 'react'
 import Styles from './form-status-styles.scss'
 
@@ -6,11 +8,17 @@ type Props = {
 }
 
 const FormStatus: React.FC<Props> = ({ context }) => {
-  const { errorState } = useContext<any>(context)
+  const { errorState, state } = useContext<any>(context)
 
   return (
     <div data-testid="status-wrapper" className={Styles.errorWrapper}>
       {errorState[0].main && <span data-testid="main-error" className={Styles.error}>{errorState[0].main}</span>}
+      {state[0].isLoading && <FontAwesomeIcon
+            data-testid="status-spinner"
+            icon={faSpinner}
+            className="spinner"
+            spin
+            size="2x" />}
     </div>
   )
 }
