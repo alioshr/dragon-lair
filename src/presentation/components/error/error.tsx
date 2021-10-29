2c6ad8e1560adfd48cfdf7504f8bd8206eefc8f1
“alioshr” <alioshmpb@gmail.com>
2021-10-29 16:20:08 -0300

    feat: add home page link option

--- src/presentation/components/error/error.tsx
+++ src/presentation/components/error/error.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Styles from './error-styles.scss'
 
 type Props = {
   error: string
-  reload: () => void
+  reload?: () => void
 }
 
 const Error: React.FC<Props> = ({ error, reload }: Props) => {
   return (
     <div className={Styles.errorWrap}>
       <span data-testid="error">{error}</span>
-      <button data-testid="reload" onClick={reload}>Tentar novamente</button>
+      {reload
+        ? <button data-testid="reload" onClick={reload}>Tentar novamente</button>
+        : <Link to="/"><span>Voltar para a página inicial</span></Link>
+      }
     </div>
   )
 }
