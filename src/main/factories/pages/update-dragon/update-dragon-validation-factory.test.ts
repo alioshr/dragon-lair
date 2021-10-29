2275b095ae6521c7135ab9810471bcb29cb7f3f2
“alioshr” <alioshmpb@gmail.com>
2021-10-29 14:09:02 -0300

    feat: add update dragon validations

---
+++ src/main/factories/pages/update-dragon/update-dragon-validation-factory.test.ts
@@ -0,0 +1,15 @@
+import { ValidationBuilder } from '@/main/builder/validation-builder'
+import { ValidatorComposite } from '@/main/composite/validator-composite'
+import { makeUpdateDragonValidations } from './update-dragon-validation-factory'
+
+jest.mock('@/main/composite/validator-composite')
+
+describe('LoginValidationFactory', () => {
+  test('ensure Validator Composite is called with the correct validations', () => {
+    makeUpdateDragonValidations()
+    expect(ValidatorComposite.build).toHaveBeenCalledWith([
+      ...ValidationBuilder.fieldName('name').required().notEqual('defaultName').build(),
+      ...ValidationBuilder.fieldName('type').required().notEqual('defaultType').min(5).build()
+    ])
+  })
+})
