411a664ce358e48e36b1939e5bcf7783cadfcd80
“alioshr” <alioshmpb@gmail.com>
2021-10-29 14:07:31 -0300

    feat: prepare page for DI

--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import '@/presentation/styles/_global.scss'
 import { Header } from '../components'
-import { UpdateDragon } from '../pages'
 
 type Props = {
   makeLogin: React.FC
   makeDragons: React.FC
+  updateDragon: React.FC
 }
 
-const Router: React.FC<Props> = ({ makeLogin, makeDragons }) => {
+const Router: React.FC<Props> = ({ makeLogin, makeDragons, updateDragon }) => {
   return (
     <Header>
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={makeDragons} />
           <Route path="/login" exact component={makeLogin} />
-          <Route path="/update/:id" exact component={UpdateDragon} />
+          <Route path="/update/:id" exact component={updateDragon} />
         </Switch>
       </BrowserRouter>
     </Header>
