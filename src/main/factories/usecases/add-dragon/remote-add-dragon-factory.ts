6daed249cfc73dbde4f30552b6e62d1b62e90292
“alioshr” <alioshmpb@gmail.com>
2021-10-30 04:06:26 -0300

    feat: addDragon factory

---
+++ src/main/factories/usecases/add-dragon/remote-add-dragon-factory.ts
@@ -0,0 +1,8 @@
+import { RemoteAddDragon } from '@/data/usecases/add-dragon/remote-add-dragon'
+import { AddDragon } from '@/domain/usecases'
+import { makeAPIUrl } from '../../http/api-url-factory'
+import { makeAxiosHttpClient } from '../../http/axios-client-factory'
+
+export const makeRemoteAddDragon = (): AddDragon => {
+  return new RemoteAddDragon(makeAPIUrl('/dragon'), makeAxiosHttpClient())
+}
