1d361be23cc9546fd27c598a3a7c17e2d8bc0efa
“alioshr” <alioshmpb@gmail.com>
2021-10-30 10:41:35 -0300

    feat: add sort

--- src/presentation/pages/dragons/components/list/list.tsx
+++ src/presentation/pages/dragons/components/list/list.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
-import DragonContext, { DragonStateTypes } from '@/presentation/contexts/dragon-context'
+import DragonContext, {
+  DragonStateTypes
+} from '@/presentation/contexts/dragon-context'
 import { DragonCard } from '..'
 import Skeleton from '../skeleton/skeleton'
 import './list-styles.scss'
@@ -7,14 +9,16 @@ import './list-styles.scss'
 const List: React.FC = () => {
   const { state } = useContext<DragonStateTypes>(DragonContext)
 
-  return <ul data-testid="dragons-list">
-  {state.dragons.map((dragon, i) => (
-    <DragonCard key={i} dragon={dragon} />
-  ))}
-  {state.isLoading && !state.id && (
-    <Skeleton />
-  )}
-</ul>
+  return (
+    <ul data-testid="dragons-list">
+      {state.dragons
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((dragon, i) => (
+          <DragonCard key={i} dragon={dragon} />
+        ))}
+      {state.isLoading && !state.id && <Skeleton />}
+    </ul>
+  )
 }
 
 export default List
