750c557cf400306ebd49be2cfdf48a911ee098b0
“alioshr” <alioshmpb@gmail.com>
2021-10-30 04:21:29 -0300

    feat: inject root composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -5,6 +5,7 @@ import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeDragons } from './factories/pages/dragons/dragons-factory'
 import { makeUpdateDragon } from './factories/pages/update-dragon/update-dragon-factory'
 import { makeDetailsPage } from './factories/pages/details/details-page-factory'
+import { makeAddDragon } from './factories/pages/add-dragon/add-dragon-factory'
 
 ReactDOM.render(
   <Router
@@ -12,6 +13,7 @@ ReactDOM.render(
   makeDragons={makeDragons}
   updateDragon={makeUpdateDragon}
   detailsPage={makeDetailsPage}
+  addDragon={makeAddDragon}
   />,
   document.getElementById('main')
 )
