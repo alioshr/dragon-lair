9b15022a29d86cafa20c3593d059ed0d8419767f
“alioshr” <alioshmpb@gmail.com>
2021-10-31 01:08:13 -0300

    feat: inject spinner in form status

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,6 +1,5 @@
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { Context, useContext } from 'react'
+import { Spinner } from '..'
 import Styles from './form-status-styles.scss'
 
 type Props = {
@@ -12,13 +11,14 @@ const FormStatus: React.FC<Props> = ({ context }) => {
 
   return (
     <div data-testid="status-wrapper" className={Styles.errorWrapper}>
-      {errorState[0].main && <span data-testid="main-error" className={Styles.error}>{errorState[0].main}</span>}
-      {state[0].isLoading && <FontAwesomeIcon
-            data-testid="status-spinner"
-            icon={faSpinner}
-            className="spinner"
-            spin
-            size="2x" />}
+      {errorState[0].main && (
+        <span data-testid="main-error" className={Styles.error}>
+          {errorState[0].main}
+        </span>
+      )}
+      {state[0].isLoading && (
+        <Spinner data-testid="status-spinner" className="spinner" />
+      )}
     </div>
   )
 }
