fb5d9bde683e6e59a88f1ee8888a53825f535832
“alioshr” <alioshmpb@gmail.com>
2021-10-31 16:51:08 -0300

    refactor: add UnexpectedError prototype

--- src/domain/errors/unexpected-error.ts
+++ src/domain/errors/unexpected-error.ts
@@ -2,5 +2,6 @@ export class UnexpectedError extends Error {
   constructor () {
     super('Ops... Algo deu errado')
     this.name = 'UnexpectedError'
+    Object.setPrototypeOf(this, UnexpectedError.prototype)
   }
 }
