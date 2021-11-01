6d227c9bc01a45d22c2269ff0ba8b8a2b099f3f4
“alioshr” <alioshmpb@gmail.com>
2021-11-01 01:31:18 -0300

    refactor: remove sut from assertions

--- src/presentation/test/form-helper.ts
+++ src/presentation/test/form-helper.ts
@@ -1,12 +1,11 @@
 import faker from 'faker'
-import { RenderResult, waitFor, fireEvent, screen } from '@testing-library/react'
+import { waitFor, fireEvent, screen } from '@testing-library/react'
 
 export const testChildCount = async (
-  sut: RenderResult,
   testId: string,
   count: number
 ): Promise<void> => {
-  const el = sut.getByTestId(testId)
+  const el = screen.getByTestId(testId)
   await waitFor(() => el)
   expect(el.childElementCount).toBe(count)
 }
@@ -20,29 +19,27 @@ export const testButtonDisabled = (
 }
 
 export const testStatusForField = (
-  sut: RenderResult,
   fieldName: string,
   wrapperStatus: string,
   errorMessage?: string
 ): void => {
-  const wrapper = sut.getByTestId(`${fieldName}-wrapper`)
-  const input = sut.getByTestId(`${fieldName}-input`)
-  const label = sut.getByTestId(`${fieldName}-label`)
+  const wrapper = screen.getByTestId(`${fieldName}-wrapper`)
+  const input = screen.getByTestId(`${fieldName}-input`)
+  const label = screen.getByTestId(`${fieldName}-label`)
   expect(wrapper.getAttribute('data-status')).toBe(wrapperStatus)
   expect(input.title).toBe(errorMessage ?? '')
   expect(label.title).toBe(errorMessage ?? '')
 }
 
 export const populateField = (
-  sut: RenderResult,
   fieldName: string,
   value: string = faker.random.word()
 ): void => {
-  const emailInput = sut.getByTestId(`${fieldName}-input`)
+  const emailInput = screen.getByTestId(`${fieldName}-input`)
   fireEvent.input(emailInput, { target: { value: value } })
 }
 
-export const clickElement = (sut: RenderResult, testId: string): void => {
-  const button = sut.getByTestId(testId) as HTMLButtonElement | HTMLLinkElement
+export const clickElement = (testId: string): void => {
+  const button = screen.getByTestId(testId)
   fireEvent.click(button)
 }
