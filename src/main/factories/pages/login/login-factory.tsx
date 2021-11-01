73b5e12561b01653570bab56751a2dd34ff956f4
“alioshr” <alioshmpb@gmail.com>
2021-11-01 01:52:01 -0300

    refactor: remove unused usecase

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Login from '../../../../presentation/pages/login/login'
-import { makeLocalSaveAccessToken } from '../../usecases/save-access-token/local-save-access-token-factory'
 import { makeLoginValidations } from './login-validation-factory'
 
 export const makeLogin: React.FC = () => (
   <Login
     validator={makeLoginValidations()}
-    saveAccessToken={makeLocalSaveAccessToken()}
   />
 )
