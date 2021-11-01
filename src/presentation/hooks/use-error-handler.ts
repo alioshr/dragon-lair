b2d162bb204cd1d21abc23e3a2532a40da2d39c8
“alioshr” <alioshmpb@gmail.com>
2021-11-01 01:43:43 -0300

    feat: add useError hook

---
+++ src/presentation/hooks/use-error-handler.ts
@@ -0,0 +1,7 @@
+
+type CallBackType = (error: Error) => void
+type ResultType = CallBackType
+
+export const useErrorHandler = (callback: CallBackType): ResultType => {
+  return (error: Error): void => callback(error)
+}
